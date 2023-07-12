f026ca989d7f8a320e04e605bee792a284de6bf2
Dmitriy Lisitsyn <lisitsin1998@mail.ru>
2023-07-12 21:48:35 +0300

    done

--- Fish.java
+++
@@ -1,8 +0,0 @@
-package ООП.Seminar1;
-
-public class Fish extends Animal{
-    public Fish(String name, Integer age, Boolean vaccination, String color,
-     String species, Owner owner, Integer legsCount, Boolean fly, Boolean run, Boolean  swim) {
-        super(name, age, vaccination, color, species, owner, legsCount, fly, run, swim);
-    }
-}
